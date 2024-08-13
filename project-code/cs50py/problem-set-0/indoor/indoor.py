88f0a5cf90725232c727572b3467c8e32a5ccf08
irfan-manzoor <rirfanm@me.com>
2024-08-14 00:46:46 +0500

    updates

--- project-code/cs50py/problem-set-0/indoor/indoor.py
+++
@@ -1,5 +0,0 @@
-#prompting user to enter text
-text = input ("Enter Text: ")
-
-#printout user input into lowercase text
-print("You entered: " +text.lower())
--- project-code/cs50py/problem-set-0/playback/playback.py
+++
@@ -1,5 +0,0 @@
-#prompt user to input text
-text = input("enter your text: ")
-
-#convert spaces to ...
-print (text.replace(" ","..."))
